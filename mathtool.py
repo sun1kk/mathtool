8d8bc4997df5ecce6365db151ef77a395f708647
sun1kk <abuasabehsanad@gmail.com>
2026-09-23 20:21:23 +0300

    Update mathtool.py

--- mathtool.py
+++ mathtool.py
@@ -16,3 +16,24 @@ if len(args) == 0 or (len(args) == 1 and args[0] == "--help"):
     print("    python mathtool.py solve                   ввод коэффициентов с клавиатуры")
     print("    python mathtool.py solve -a 1 -b -3 -c 2   решение с заданными коэффициентами")
     sys.exit(0)
+
+# Проверка команды solve
+if args[0] != "solve":
+    print("ОШИБКА: неизвестная команда", file=sys.stderr)
+    sys.exit(1)
+
+raw_a = ""
+raw_b = ""
+raw_c = ""
+
+# Если запустили просто "python mathtool.py solve"
+if len(args) == 1:
+    raw_a = input("Введите A: ")
+    raw_b = input("Введите B: ")
+    raw_c = input("Введите C: ")
+# Если передали коэффициенты через флаги -a, -b, -c
+elif len(args) == 7:
+    if args[1] == "-a" and args[3] == "-b" and args[5] == "-c":
+        raw_a = args[2]
+        raw_b = args[4]
+        raw_c = args[6]
