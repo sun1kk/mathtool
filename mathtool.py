358c4678c0e045b8191b157b4ca4bcc16b0213e5
sun1kk <abuasabehsanad@gmail.com>
2026-09-23 20:02:52 +0300

    Update mathtool.py

--- mathtool.py
+++ mathtool.py
@@ -7,3 +7,12 @@ MAX_VALUE = 10000
 # 1. Получаем аргументы из командной строки
 args = sys.argv[1:]
 
+# Проверка на справку
+if len(args) == 0 or (len(args) == 1 and args[0] == "--help"):
+    print("mathtool — решение уравнений вида A*x^2 + B*x + C = 0")
+    print("Использование:")
+    print("    python mathtool.py                         вывод справки")
+    print("    python mathtool.py --help                  вывод справки")
+    print("    python mathtool.py solve                   ввод коэффициентов с клавиатуры")
+    print("    python mathtool.py solve -a 1 -b -3 -c 2   решение с заданными коэффициентами")
+    sys.exit(0)
